5d450cb08d38c04cd6253eb918713cc568b05e10
Nazia09-p <63148887+Nazia09-p@users.noreply.github.com>
2026-01-22 11:43:12 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    let slides = document.getElementsByClassName("slides");
+
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
+
+  // Mobile menu toggle
+  window.toggleMenu = function() {
+    const nav = document.getElementById("navbar");
+    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+  }
+});
+
+
